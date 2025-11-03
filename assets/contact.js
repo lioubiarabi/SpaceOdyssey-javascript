634c101ed760de7e60daf67a43cc5f2282c7c388
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-11-03 11:36:55 +0100

    validating all the form inputs

--- assets/contact.js
+++ assets/contact.js
@@ -7,5 +7,17 @@ let phone = document.getElementById("phone");
 let message = document.getElementById("message");
 
 form.addEventListener("input", ()=>{
-    firstName.value && (console.log(/^[a-z]{3,20}$/i.test(firstName.value)));
+    firstName.value && (validate(firstName, /^[a-z]{3,20}$/i));
+    lastName.value && (validate(lastName, /^[a-z]{3,20}$/i));
+    email.value && (validate(email, /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/));
+    phone.value && (validate(phone, /^(\+)?\d{10,}$/));
+    message.value && (validate(message, /\S{2,}/));
 })
+
+function validate(input, regex) {
+    if(regex.test(input.value)) {
+        input.style.borderColor = "#e5e7eb"
+    } else {
+        input.style.borderColor = "red"
+    }
+}
